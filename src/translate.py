043e49bd361f4e42f1d7530a089fd8445bd53990
Valery Klachkov <reserfodium.none@gmail.com>
2017-12-14 18:46:46 +1000

    Minor changes | v1.1

--- src/translate.py
+++ src/translate.py
@@ -5,13 +5,23 @@
 # Dependencies:
 #  > yandex.translate
 
+BOLD = "\033[1m"
+DEF = "\033[0m"
+N = "\n"
+
 from sys import argv, exit
-from yandex_translate import YandexTranslate
+
+try:
+    from yandex_translate import YandexTranslate
+except:
+    print(f'{N}YandexTranslate {BOLD}not found{DEF}{N}')
+    exit()
+
 
 API_KEY = "trnsl.1.1.20171205T125702Z.c9a3990460e7cb48.2c2029147b887744cb59ae10b64f8fcd8472d698"
 
 if 2 > len(argv):
-    print("\ntranslate.py [lang] [what]\n")
+    print("{N}translate.py [lang] [what]{N}")
     exit()
 else:
     # In which language
@@ -25,4 +35,4 @@ translate = YandexTranslate(API_KEY);
 translated = translate.translate(what, to)['text'][0];
 
 # Print
-print(f"\n{translated}\n");
+print(f"{N}{translated}{N}");
